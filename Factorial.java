06f546643cce6b344dcede792336740479fcc1e5
nishantverma8934-web <nishantverma8934@gmail.com>
2025-11-02 01:33:20 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+public class lefthandpyramid{
+    public static void main(string[]args){
+        int rows = 5;
+        for(int i = 1;i<=rows; i++){
+            for (j = 1; j <= i; ++j) {
+                system.out.print("*");
+            }
+            system.out.println();
+        }
+    }
+}
