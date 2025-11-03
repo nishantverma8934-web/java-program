5afd445b7673f35b5e15023f15e3e7acea8c0e28
nishantverma8934-web <nishantverma8934@gmail.com>
2025-11-03 16:07:24 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic{
+    public static void main(String[]args){
+        int a = 10,b = 5;
+        int addition = a+b;
+        int subtraction =a-b;
+        int multiplication =a*b;
+        int division =a/b;
+        int modulus =a%b;
+        System.out.println("Arithmetic Operators:");
+        System.out.println("Addition:"+addition);
+        System.out.println("Multiplication:"+multiplication);
+        System.out.println("Division:"+division);
+        System.out.println("Modulus:"+modulus);
+    }
+}
